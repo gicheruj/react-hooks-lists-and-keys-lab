c906f905fc383f950c85e009577226ca000e54ae
John <jgicheru325@gmail.com>
2024-05-06 12:55:35 +0300

    Lists and Keys lab

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -1,14 +1,21 @@
+// ProjectList.js
 import React from "react";
 import ProjectItem from "./ProjectItem";
 
 function ProjectList({ projects }) {
-  console.log(projects);
   return (
-    <div id="projects">
-      <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+    <div className="project-list">
+      {projects.map((project, index) => (
+        <ProjectItem
+          key={index}
+          name={project.name}
+          about={project.about}
+          technologies={project.technologies} // Make sure to pass the technologies prop
+        />
+      ))}
     </div>
   );
 }
 
 export default ProjectList;
+
